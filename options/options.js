5e9306ba743329b691e46c545265c0b44c0a507b
Crowdbotics <team@crowdbotics.com>
2021-08-08 22:44:48 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "galaxy_reviews_29643Identifier",
+  url: "https://galaxy_reviews_29643Identifier.botics.co",
+  api: "https://galaxy_reviews_29643Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
